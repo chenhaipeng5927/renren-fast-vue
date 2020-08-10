269fd73869a0dbe668cebb785d28dd631268c2c6
chenhp <c2432190707@qq.com>
2020-08-10 18:33:40 +0800

    删除增加分类样式

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,6 +1,25 @@
 <!--  -->
 <template>
-  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree
+    :data="data"
+    :props="defaultProps"
+    :expand-on-click-node="false"
+    show-checkbox
+    node-key="catId"
+  >
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button v-show="node.level <= 2" type="text" size="mini" @click="() => append(data)">增加</el-button>
+        <el-button
+          v-if="node.childNodes.length==0"
+          type="text"
+          size="mini"
+          @click="() => remove(node, data)"
+        >删除</el-button>
+      </span>
+    </span>
+  </el-tree>
 </template>
 <script>
 export default {
@@ -10,8 +29,8 @@ export default {
       data: [],
       defaultProps: {
         children: "children",
-        label: "name",
-      },
+        label: "name"
+      }
     };
   },
   // 测试
@@ -22,18 +41,26 @@ export default {
     getMenus() {
       this.$http({
         url: this.$http.adornUrl("/product/category/list/tree"),
-        method: "get",
+        method: "get"
       }).then(({ data }) => {
         const { data: menu } = data;
         console.log("成功！", menu);
         this.data = menu;
       });
     },
+    append(data) {
+      console.log("append", data);
+    },
+
+    remove(node, data) {
+      console.log("remove", node);
+      console.log("data", data);
+    }
   },
   // 测试
   created() {
     this.getMenus();
-  },
+  }
 };
 </script>
 <style scoped>
